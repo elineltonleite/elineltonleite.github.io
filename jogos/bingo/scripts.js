4fa10eeffdf228f52adf72035d8cbfe6738f8803
elineltonleite <elineltonleite@gmail.com>
2021-09-25 18:59:05 -0300

    add projeto bingo ao I.O
    de volta ao git hub

---
+++ jogos/bingo/scripts.js
@@ -0,0 +1,51 @@
+ var historico=[] 
+
+ function resultado(){
+	numSorteado = parseInt(Math.floor(Math.random() * 90)+1)
+	
+	if(historico.includes(numSorteado)){
+		//alert('numero '+numSorteado+'já foi sorteado')
+		numSorteado = parseInt(Math.floor(Math.random() * 90)+1)
+		resultado()
+	}
+	
+	historico.push(numSorteado)
+	//console.log(historico)
+	
+	montarQuadro(historico)
+	document.getElementById('num-sorteado').innerHTML= numSorteado
+
+ }
+document.getElementById("btn-sortear").onclick = resultado
+
+
+var writeHtml ="", i;
+var letras =['B','I','N','G','O'];
+for(i = 0; i < 5; i++){
+	writeHtml += '<div class="bola cor-'+i+'"><div class="letras">'+letras[i]+ '</div></div>';
+}
+document.getElementById("topo").innerHTML=writeHtml;
+
+function montarQuadro(result){
+	var num = ""
+	
+	for(j = 1; j <= 90; j++){
+		
+		if(result.includes(j)){
+			bg = "red"
+		}else{
+			bg ="#cecece;color:black"
+		}
+	
+		if(j<10){
+			num +='<div class="bola-menor" style="background:'+bg+'"><div class="numero">0'+j+ '</div></div>';
+		}else{
+			num +='<div class="bola-menor" style="background:'+bg+'"><div class="numero">'+j+ '</div></div>';
+		}	
+	
+	}
+	document.getElementById("quadro").innerHTML=num;
+}
+montarQuadro()
+
+
